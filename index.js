7a47ee806b1ebd5583e46eaf398f13a6d4bbe968
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-09 14:14:40 -0500

    clarify creation error message & add placeholder handlers for stats and redirects

--- index.js
+++ index.js
@@ -13,17 +13,24 @@ app.post('/create', (req, res) => {
   const schema = Joi.object().keys({
     // quick-and-dirty URL validation
     url: Joi.string().regex(/https?:\/\/(www\.)?[a-z0-9\.]/i).required(),
-    custom: Joi.string().alphanum()
+    custom: Joi.string().alphanum().not('create')
   })
   if(Joi.validate(req.body, schema).error === null) {
     // make the shortening request
   } else {
     res.status(400).send({
       response: 400,
-      message: "Your request must be a JSON object with two properties: url and custom. url is required and must be a url. Custom is optional."
+      message: "Your request must be an object with the properties url and custom. url is required and must be a url. Custom is optional, but may not have the value 'create'."
     })
   }
+})
 
+app.get('/.*/stats', (req, res) => {
+  // handle requests for stats on a link
+})
+
+app.get( (req, res) => {
+  // handle actual URL redirects
 })
 
 app.listen(port, () => {
