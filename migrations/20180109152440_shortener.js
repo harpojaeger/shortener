4c00eaa112c32ab49d9bf7d2b2be1aefa2d8ca29
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-09 15:25:43 -0500

    clarify structure of requests table

---
+++ migrations/20180109152440_shortener.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.dropTable('links')
+  .dropTable('requests')
+  .createTable('links', table => {
+    table.increments('id')
+    table.string('url').notNullable()
+    table.timestamp('created').defaultTo(knex.fn.now())
+    table.string('short').unique()
+  })
+  .createTable('requests', table => {
+    table.increments('id')
+    table.string('slug')
+    table.timestamp('timestamp').defaultTo(knex.fn.now())
+    table.string('result')
+    table.string('matched_id')
+    table.string('method')
+  })
+  // we have to return a Promise after chaining, to make knex:migrate happy
+  .then(() => {})
+
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('links').dropTable('requests').then(() => {})
+};
