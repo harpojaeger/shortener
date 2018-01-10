a43d8fdd953bc8df6da90dfa7d822aac0ca8327a
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-10 12:01:22 -0500

    reset id seqs before seeding test data

--- seeds/test/links.js
+++ seeds/test/links.js
@@ -3,11 +3,16 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('links').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('links').insert([
-        {url: 'http://harpojaeger.com', short: 'themostinterestingblogintheworld'},
-        {url: 'http://google.com', short: 'dontbeevil'},
-        {url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', short: 'important'},
-      ]);
+      // Reset the id sequence
+      return knex.raw('alter sequence links_id_seq restart with 1')
+      .then( () => {
+        // Inserts seed entries
+        return knex('links')
+        .insert([
+          {url: 'http://harpojaeger.com', short: 'themostinterestingblogintheworld'},
+          {url: 'http://google.com', short: 'dontbeevil'},
+          {url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', short: 'important'},
+        ]);
+      })
     });
 };
---
+++ seeds/test/requests.js
@@ -0,0 +1,18 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('requests').del()
+    .then(function () {
+      // reset the id sequence
+      return knex.raw('alter sequence requests_id_seq restart with 1')
+      .then( () => {
+        // Seed db with 30 successful requests for shortlink 5
+        return knex('requests').insert(Array(30).fill({
+          slug: 'important',
+          result: 302,
+          matched_id: 3,
+          method: 'slug_string'
+        }))
+      })
+    })
+}
